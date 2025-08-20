43e1cda5e4ec7eba126ba3a39f69d8251408e17d
edwingsantos <edwing.santos@ucas-edu.net>
2025-08-20 09:51:43 -0600

    hello_world.py

---
+++ Notes/inputs.py
@@ -0,0 +1,15 @@
+# ES 1st Input and Output notes
+
+#5 what is an input
+#A way for the code to get information
+
+#6 inputs are important because you could get new info
+
+name = input("what is your name: ")
+#  ^   ^        ^
+# Save the input where it can be accessed agian
+# Use keyword "input"
+# Inside parens give user instructions
+
+# Both instruction methos are fine 
+print("hello",name,"how are you doing")
---
+++ practices/hollo_world.py
@@ -0,0 +1,3 @@
+# ES 1ts hello_world
+name = input("hello, what is your name: ")
+print("hello",name,",nice to meet you")
