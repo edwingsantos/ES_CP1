bbf3024bec7eb81b75a375cd0b1185717c71c022
edwingsantos <edwing.santos@ucas-edu.net>
2025-08-22 09:47:42 -0600

    who_are_you.py

---
+++ Independent-work/who_are_you.py
@@ -0,0 +1,6 @@
+# ES 1rst who are you 
+name = input("Hello, what is your name?: ")
+age = input("Hi nice to meet you, how old are you?: ")
+fav_color = input("Nice, what is your favorite color?: ")
+
+print("Well",name,",you seem really cool,",age,"is such a good age, same as the color",fav_color,".")
