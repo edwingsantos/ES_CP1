9b2029c3f591b0e74bc493dc8dfc5af66eec9f38
edwingsantos <edwing.santos@ucas-edu.net>
2025-08-27 09:52:42 -0600

    numbers.py

--- Notes/numbers.py
+++ Notes/numbers.py
@@ -1,1 +1,23 @@
-# ES 1rst numbers notes
+# ES 1rst integers and floats notes
+
+age = 99999999999999
+year = 2015
+room = 200
+hour = 9
+
+#Float (floating point number)
+pi = 3.14
+percent = .75
+damage = 9.75
+
+#Math 
+print(5+2)
+print(5-2)
+print(5*2)
+print(5/2)
+print(5//2)
+print(5%2)
+print(5**2)
+
+#type casting 
+int()
