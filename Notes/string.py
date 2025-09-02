74cd638948c45f776ed9ce35b6bc86176a25bea6
edwingsantos <edwing.santos-ucas-edu.net>
2025-09-02 09:52:52 -0600

    string.py

---
+++ Notes/string.py
@@ -0,0 +1,15 @@
+#ES 1 string notes
+
+sentence = "The quick brown fox jumps over the lazy dog!"
+
+first_name = "edwing"
+last_name = "santos"
+month = "september"
+lenght = len(sentence)
+print('"hi"is hi')
+print("'hi'is hi")
+
+full_name = first_name + " Santos"
+full_name = first_name + " " + last_name
+print(full_name)
+print(last_name[0:3])
