aec6634820bb616dc8582dc2947c9ba197740c66
edwingsantos <edwing.santos-ucas-edu.net>
2025-09-03 09:52:01 -0600

    maldlib.py

---
+++ Independent-work/madlib.py
@@ -0,0 +1,20 @@
+#ES 1 madlib project
+
+adjective_1 = input("Can you say an adjective?: ")
+adjective_2 = input("Can you say an adjective?: ")
+type_of_bird = input("Can you say a type of bird?: ")
+room_in_a_house = input("Can you say a room in a house?: ")
+verb_past_tense = input("Can you say a verb ending in a past tense?:")
+verb = input("Can you say a verb?: ")
+relative_name = input("Can you say a relatives name?: ")
+noun_1 = input("Can you say a noun?:")
+verb_ending_ing_1 = input("Can you say a verb ending in ing?: ")
+liquid = input("Can you say a liquid?: ")
+part_of_body = input("Can you say a part of the body (plural)?:")
+plural_noun = input("Can you say a plural noun?: ")
+verb_ending_ing_2 = input("Can you say a verb ending with ing?: ")
+noun_2 = input("Can you say a noun?: ")
+
+messege = "It was a " + adjective_1 + ", cold November day. I woke yp to the " + adjective_2 + " smell of " + type_of_bird + " roasting in the " + room_in_a_house + " downstairs. I " + verb_past_tense + " down the stairs to see if I could help " + verb + " the dinner. My mom said, 'see if " + relative_name + " needs a fresh " + noun_1 + " '. So I carried a tray of glasses full off " + liquid + " into the " + verb_ending_ing_1 + " room. When I got there, I couldnt believe my  " + part_of_body + "! There were " + plural_noun + verb_ending_ing_2 + " on the " + noun_2 + "!."
+
+print(messege)
