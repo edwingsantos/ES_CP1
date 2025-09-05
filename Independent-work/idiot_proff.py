0ad1cfa3dd9422275f2ca9852c2671143f30a327
edwingsantos <edwing.santos-ucas-edu.net>
2025-09-05 09:52:52 -0600

    idiot_proff.py

---
+++ Independent-work/idiot_proff.py
@@ -0,0 +1,7 @@
+#ES 1 idiot proof
+
+name = input("Hello what is your name?: ").title( )
+phone_number = int(input("Hello " + name + ", what is your phone number?: "))
+gpa = float(input("What is your GPA?: "))
+
+print(gpa + name + phone_number)
