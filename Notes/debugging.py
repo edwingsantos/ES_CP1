45934fdcd715b666515894b7929acf12a5f8404f
edwingsantos <edwing.santos@ucas-edu.net>
2025-09-26 09:52:35 -0600

    fix_code.py

--- Notes/debugging.py
+++ Notes/debugging.py
@@ -1,4 +1,8 @@
 #ES 1 debugging.py
-#we call it debugging because we call buggs progblems 
+#we call it debugging because we call buggs progblem
 
 
+import random
+denominator = random.randint(0,5)
+
+print(10/denominator)
