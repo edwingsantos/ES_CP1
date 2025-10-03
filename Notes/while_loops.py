e3662273a5ac3bc27e714c55ad578377dfbbfb13
edwingsantos <edwing.santos@ucas-edu.net>
2025-10-03 09:48:47 -0600

    while_loops.py

---
+++ Notes/while_loops.py
@@ -0,0 +1,28 @@
+#ES 1 while loop notes
+
+#num is the derator, it tells us how many tames the loop is been going 
+for num in range(1,20):
+    print(num)
+import time
+
+
+import random
+goose = random.randint(1,20)
+count = 1
+
+while True:
+    if count == goose:
+        break
+    else:
+        print("duck")
+print("goose")
+
+
+
+
+i = 0
+while i < 20:
+    if i ==10:
+        print("i is 10")
+
+
