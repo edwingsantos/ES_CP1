bb14fdeafdf5ea9c518535105cac000510fec200
edwingsantos <edwing.santos@ucas-edu.net>
2025-10-03 09:09:56 -0600

    multiplication_table.py

--- Independent-work/multiplication_table.py
+++ Independent-work/multiplication_table.py
@@ -1,1 +1,12 @@
 #ES 1 multiplication table 
+
+many = int(input("What do you want to be the range?: "))
+
+multipication = 1
+
+for num in range(1,many,+1):
+    number = []
+    for i in range(1,13):
+        number.append(i*multipication)
+        multipication += 1
+        print(num)
