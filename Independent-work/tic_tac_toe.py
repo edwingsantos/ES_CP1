c40dd831a57f33815e5ae834dbe02a3bdc3c2c23
edwingsantos <edwing.santos@ucas-edu.net>
2025-10-09 09:46:14 -0600

    tic_tac_toe.py

---
+++ Independent-work/tic_tac_toe.py
@@ -0,0 +1,13 @@
+#ES 1 tic tac toe
+
+#First i will import random into the code so I will be able to use it later.
+#Then I will make a variable name "board", put an equal sig, then put brackens two times, and write the numbers 1-9, but every three press enter. 
+#I will use the while True loop, so I dont have to put the code multiple times, with a : so everything underneath it is true or it will move on.
+#After I will make a variable named players_choice, and make it an input to see what number the user wants.
+#Then I will use board [players_choice] = x, becuase the user will be x 
+#After i will make another variable named computers_choice = random.randint(1,9), that will be the computers choice.
+#Then i will write [computers_choice] = O, becuase the computer is O
+#After the board is full I will make an if statement, if 1,2,3 or 4,5,6 or 7,8,9, or 1,4,7 or 2,5,8 or 3,6,9 or 1,5,9 or 7,5,9 = x then the player wins and print win 
+#but if  1,2,3 or 4,5,6 or 7,8,9, or 1,4,7 or 2,5,8 or 3,6,9 or 1,5,9 or 7,5,9 = O then the print loose 
+#then i will put a break statment,
+#but if non of the above happens, i will make and else statement printing tie
