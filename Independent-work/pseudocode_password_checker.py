c5d4228a315e98b46f16f0a3ba8247c2fa165d31
edwingsantos <edwing.santos@ucas-edu.net>
2025-10-14 09:53:39 -0600

    pseudocode_password_checker.py

--- Independent-work/pseudocode_password_checker.py
+++ Independent-work/pseudocode_password_checker.py
@@ -8,50 +8,77 @@
 #print saying the instruction for the password, and the steps required for it 
 print("Hi, you are going to make a password, you need at least 8 characters long, contains at least one uppercase letter,  contains at least one lowercase letter, contains at least one number and contains at least one special character (!@#$%^&*()_+-=[]{}|;:,.<>?)")\
 #make a list that says the reqired lenght sould be 8
-required_lenght = 8
-#make a while loop, while True:, press enter
-while True:
-#name a varible named user_password, equals input("What would you like your password to be?: ")
-    user_password = input("What would you like your password to be?: ")
-#make an if statement asking the program that if user_password has a lower case using the method islower. 
-    if user_password.islower():
+
+
+#make a list fo the special chraracters 
+special_characters = "!""@""#""$""%""^""&""*""()""_""+""-""=""[]""{}""|"";"":"","".""<"">""?"
+
+
+#score is going to keep track of my score
+score = 0
+#make the password an input so the user can type
+password = input("What would you like your password to be: ")
+#Put N as password so i can make a loop later
+N = password 
+#Use lenght to check if it meet the lenght requiremment
+lenght = len(password)
+#make an if statement to check if the lenght is enough for 8 chracters
+if lenght >= 8:
+#print that you have met the lenght requirement if the if stement is true
+    print("You have the lenght requirement")
+#add one point to you score
+    score+=1 
+#make an if statemtnt if is not true
+else:
+    print("You didn't meet the lenght requirement")
+
+
+#make a for loop using N in password
+for N in password:
+#check if N is lower case by saying is not true 
+    if N.islower() != True:
 #print that you have a lower case if the input is true
         print("you have a lower case!")
+#add a point to your score
+        score+=1
 #Then break so the code stops
         break
-#If the top if not true, the else statment will fix it 
+#make an else statement if is not true
     else:
-        print("You dont have a lower case , please enter a lower case letter")
-#write continue so it goes back to the begging of the loop 
-        continue
-
-#make another while loop fo upper case this thime
-while True:
-    user_password = input("What would you like your password to be?: ")
-# make an if stament asking the progam tht if the password has a upper case 
-    if user_password > required_lenght:
-#if true, it will print it has upper case
-        print("You have met the require lenght!")
-#break so code doessnt run for ever
-        break
-#make an else statment if password not true
-    else:
-        print("You havent met the lenght requirent")
-        continue  
-
-#do evething it agian for letter lenght
-while True:
-#name a varible named user_password, equals input("What would you like your password to be?: ")
-    user_password = input("What would you like your password to be?: ")
-#make an if statement asking the program that if user_password has a lower case using the method islower. 
-    if user_password:
-#print that you have a lower case if the input is true
-        print("you have a lower case!")
+        print("You dont have a lower case")
+for N in password:
+#make another if statemnt for upper case now 
+    if password.isupper():
+        print("you have a upper case")
+#add a point to your score
+        score+=1
 #Then break so the code stops
         break
-#If the top if not true, the else statment will fix it 
+#make an else statement if is not true    
     else:
-        print("You dont have a lower case , please enter a lower case letter")
-#write continue so it goes back to the begging of the loop 
-        continue
+        print("You dont have a upper case")
 
+for N in password:
+#Now make one but for special characters
+#check if password is on my list
+    if password in special_characters:
+        print("You have one of the special chracters")
+        #add one point 
+        score+=1
+        #break so code doesnt go for ever
+        break
+#make an else statement if is not true
+    else:
+        print("You dont have a special chracter")
+for N in password:
+#now make one for the number
+#check if there is a number in my password
+    if password.isdigit():
+        print("You have a number")
+        #add a point
+        score+=1
+        #break so code doesnt go for ever
+        break
+    #make an else statement if is not true
+    else:
+        print("You dont have a number")
