3cdbc60adc9e56035e9e488062c2645787e885d2
edwingsantos <edwing.santos@ucas-edu.net>
2025-10-23 09:51:05 -0600

    caesar_cipher_encoder.py

--- Independent-work/caesar_cipher_encoder.py
+++ Independent-work/caesar_cipher_encoder.py
@@ -4,7 +4,7 @@
 #make an input for the user to choose a letter 
 letter = input("choose a lower case letter: ")
 #make the text variable and make the letter input = ord(letter), so it turns into a number 
-text = (f"{letter} = {ord(letter)}")
+text = ord(letter)
 #make a num variable and make it a interger, then make text = chr(text)
-(f"{text} = {int(chr(text))}")
-print(text)
+num = (f"{text} = {chr(text)}")
+print(num)
