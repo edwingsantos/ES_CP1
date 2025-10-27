3143862cc4ee3391c7a04c8da8966083d5e684f0
edwingsantos <edwing.santos@ucas-edu.net>
2025-10-27 09:53:11 -0600

    combat_funtion.py

--- Independent-work/combat_funtion.py
+++ Independent-work/combat_funtion.py
@@ -1,35 +1,33 @@
 #ES 1 combat fucntion 
 import random 
-def player_choice():
+
 print("Welcome to training! First I need to know some things about you!")
 input("what is your name?: ")
-class_fighter = input("What class of fighter are you?" \
+class_fighter = int(input("What class of fighter are you?" \
 "1 if you are a fighter " \
 "2 if you are a Mage " \
-"3 if you are a Rouge: " )
-while True:
+"3 if you are a Rouge: " ))
+
+def class_fighter(fighter):
     if class_fighter == 1:
-        print("You are a fighter, here are your stats: " \
+            print("You are a fighter, here are your stats: " \
         "Health: 30 " \
         "Damage: 23 ")
-        break
-    elif class_fighter == 2:
+def class_fighter(Mage):
+    if class_fighter == 2:
          print("You are a Mage, here are your stats: " \
         "Health: 35 " \
         "Damage: 19")
-         break
-    else:
+def class_fighter(fighter):
+    if class_fighter == 3:
          print("You are a Rouge, here are your stats: " \
         "Health: 45 " \
         "Damage: 15" )
-         break
     
-scenario = random.randint(1,5)
+order = random.randint(1,2)
 
 
-if scenario == 1:
-    print("The scenario is the hunted cave: Opponet is batman " \
-    "Health 45 Damage: 25")
-    
-
-
+if order == 1:
+     print("A Monster is attacking you, quick choose your attack ")
+else:
+     print("A Moster is attacking ")
