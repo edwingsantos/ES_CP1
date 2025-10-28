9cead2670937a8e0803873af32c035ac20d84821
edwingsantos <edwing.santos@ucas-edu.net>
2025-10-28 09:51:26 -0600

    libary_builtinfuntions.py

---
+++ Notes/libary_builtinfuntions.py
@@ -0,0 +1,37 @@
+#ES 1 libary built in functions 
+import turtle as t
+import random
+colors = ["orange","green","blue","purple","red"]
+side = random.randint(10,500)
+
+t.color(random.choice(colors))
+t.shape("turtle")
+t.shapesize(12)
+t.fillcolor(random.choice(colors))
+
+t.begin_fill()
+for x in range(1,4):
+    t.forward(side)
+    t.right(90)
+t.end_fill()
+
+t.penup()
+t.goto(50,50)
+t.pendown()
+
+t.fillcolor("orange")
+t.begin_fill()
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.end_fill()
+
+
+
+
+t.done()
