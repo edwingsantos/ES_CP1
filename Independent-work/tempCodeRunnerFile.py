6f99cf17f8998350201141b918cd834a870f8b0f
edwingsantos <edwing.santos@ucas-edu.net>
2025-10-30 09:52:58 -0600

    turtle_race.py

--- Independent-work/tempCodeRunnerFile.py
+++ Independent-work/tempCodeRunnerFile.py
@@ -1,1 +1,10 @@
-t2.speed(speed)
+if t1 == end:
+    print("red won")
+elif t2 == end:
+    print("purple")
+elif t3 == end:
+    print("yellow")
+elif t4 == end:
+    print("blue")
+elif t5 == end:
+    print("orange")
--- Independent-work/turtle_race.py
+++ Independent-work/turtle_race.py
@@ -25,7 +25,7 @@ t5.shape("turtle")
 
 t1.hideturtle()
 t1.penup()
-t1.goto(0,50)
+t1.goto(-400,50)
 t1.pendown()
 t1.showturtle()
 
@@ -33,48 +33,59 @@ t1.showturtle()
 
 t2.hideturtle()
 t2.penup()
-t2.goto(0,70)
+t2.goto(-400,70)
 t2.pendown()
 t2.showturtle()
 
 
 t3.hideturtle()
 t3.penup()
-t3.goto(0,90)
+t3.goto(-400,90)
 t3.pendown()
 t3.showturtle()
 
 
 t4.hideturtle()
 t4.penup()
-t4.goto(0,110)
+t4.goto(-400,110)
 t4.pendown()
 t4.showturtle()
 
 t5.hideturtle()
 t5.penup()
-t5.goto(0,130)
+t5.goto(-400,130)
 t5.pendown()
 t5.showturtle()
 
-while True:
+end = t.Turtle()
 
-    t1.forward(random.randint(10,59))
+end.hideturtle()
+end.penup()
+end.goto(500,600)
+end.pendown()
+end.sety(-600)
+end.showturtle()
+
+speed = random.randint(100,200)
+
+for num in range(speed):
+    t1.forward(random.randint(50,100))
+    t2.forward(random.randint(50,100))
+    t3.forward(random.randint(50,100))
+    t4.forward(random.randint(50,100))
+    t5.forward(random.randint(50,100))
+    x1 = t1.xcor()
+
+if t1 == end:
+    print("red won")
+elif t2 == end:
+    print("purple")
+elif t3 == end:
+    print("yellow")
+elif t4 == end:
+    print("blue")
+elif t5 == end:
+    print("orange")
 
 
-    t2.forward(random.randint(10,59))
-
-
-    t3.forward(random.randint(10,59))
-
-
-    t4.forward(random.randint(10,59))
-
-
-    t5.forward(random.randint(10,59))
-
-
-
-
-
-    t.done()
+t.done()
