5f63227b9f8d9d9de9fa6af0d3ff8ff730974daa
edwingsantos <edwing.santos@ucas-edu.net>
2025-11-03 09:52:02 -0700

    maze.py

--- Independent-work/maze.py
+++ Independent-work/maze.py
@@ -4,5 +4,24 @@ import turtle as t
 
 m = t.Turtle()
 
+#make the outline of the maze
+m.left(90)
+m.forward(600)
+m.left(90)
+m.forward(550)
 m.penup()
-m
+m.forward(50)
+m.left(90)
+m.pendown()
+m.forward(600)
+m.left(90)
+m.forward(550)
+m.hideturtle
+t.done
+#make the inside of the maze 
+
+s = [] = t.Turtle
+
+s
+
+
