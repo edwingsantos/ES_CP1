d1a73e610fcdccf501b9bd3e5ad45050a12fd6e8
edwingsantos <edwing.santos@ucas-edu.net>
2025-11-06 09:51:54 -0700

    maze.py

--- Independent-work/maze.py
+++ Independent-work/maze.py
@@ -5,7 +5,9 @@ import random
 import turtle as t 
 #make a varable called m for outline of the maze, with t.turtle
 m = t.Turtle()
-
+wn = t.Screen()
+wn.setup(1300,1300)
+m.speed("fastest")
 #make the outline of the maze
 m.left(90)
 m.forward(600)
@@ -41,23 +43,131 @@ grid_columbs = [[random.randint(0,1),random.randint(0,1),random.randint(0,1),ran
              [random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1)]
              ]
 #make a varable called rock that equals 1
-rock = 1
-#make a while loop that is rock is less than 6 then the code would run
-while rock < 6:
-    #let the turlte go right 90*
-    m.right(90)
+m.right(90)
     #and forward 100
-    m.forward(100)
+m.forward(500)
+m.right(90)
+#make a while loop that is rock is less than 6 then the code would run
+x=0
+while True:
+    if x == 6:
+        break
+    #let the turlte go right 90*
+
     #if the random number is 1 is pendown and forward 100
-    if grid_rows == 1:
+    if grid_rows[0][x] == 1:
         m.pendown()
         m.forward(100)
         #and make it stop once it is at the end where the line is 
-        if m
-    #make this again but for columbs
-    if grid_columbs == 0:
+
+    if grid_rows[0][x] == 0:
         m.penup()
         m.forward(100)
+        #and make it stop once it is at the end where the line is 
+    x += 1
+    #make this again but for columbs
+m.right(90)
+m.forward(100)
+m.right(90)
+x=0
+while True:
+    if x == 6:
+        break
+    #let the turlte go right 90*
+
+    #if the random number is 1 is pendown and forward 100
+    if grid_rows[1][x] == 1:
+        m.pendown()
+        m.forward(100)
+        #and make it stop once it is at the end where the line is 
+
+    if grid_rows[1][x] == 0:
+        m.penup()
+        m.forward(100)
+    x += 1
+    
+m.left(90)
+m.forward(100)
+m.left(90)
+x=0
+while True:
+    if x == 6:
+        break
+    #let the turlte go right 90*
+
+    #if the random number is 1 is pendown and forward 100
+    if grid_rows[2][x] == 1:
+        m.pendown()
+        m.forward(100)
+        #and make it stop once it is at the end where the line is 
+
+    if grid_rows[2][x] == 0:
+        m.penup()
+        m.forward(100)
+    x += 1
+
+m.right(90)
+m.forward(100)
+m.right(90)
+x=0
+while True:
+    if x == 6:
+        break
+    #let the turlte go right 90*
+
+    #if the random number is 1 is pendown and forward 100
+    if grid_rows[3][x] == 1:
+        m.pendown()
+        m.forward(100)
+        #and make it stop once it is at the end where the line is 
+
+    if grid_rows[3][x] == 0:
+        m.penup()
+        m.forward(100)
+    x += 1
+
+m.left(90)
+m.forward(100)
+m.left(90)
+x=0
+while True:
+    if x == 6:
+        break
+    #let the turlte go right 90*
+
+    #if the random number is 1 is pendown and forward 100
+    if grid_rows[4][x] == 1:
+        m.pendown()
+        m.forward(100)
+        #and make it stop once it is at the end where the line is 
+
+    if grid_rows[4][x] == 0:
+        m.penup()
+        m.forward(100)
+    x += 1
+
+m.right(90)
+m.forward(100)
+m.right(90)
+m.forward(100)
+m.right(90)
+x=0
+while True:
+    if x == 6:
+        break
+    #let the turlte go right 90*
+
+    #if the random number is 1 is pendown and forward 100
+    if grid_columbs[0][x] == 1:
+        m.pendown()
+        m.forward(100)
+        #and make it stop once it is at the end where the line is 
+
+    if grid_columbs[0][x] == 0:
+        m.penup()
+        m.forward(100)
+    x += 1
+
 
 
 t.done()
--- Independent-work/tempCodeRunnerFile.py
+++ Independent-work/tempCodeRunnerFile.py
@@ -1,10 +1,1 @@
-if t1 == end:
-    print("red won")
-elif t2 == end:
-    print("purple")
-elif t3 == end:
-    print("yellow")
-elif t4 == end:
-    print("blue")
-elif t5 == end:
-    print("orange")
+[0]
