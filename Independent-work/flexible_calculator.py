a0889642b83e15964c5e594a5628e5f1680a7332
edwingsantos <edwing.santos@ucas-edu.net>
2025-11-18 09:52:38 -0700

    flexible_calculator.py

--- Independent-work/flexible_calculator.py
+++ Independent-work/flexible_calculator.py
@@ -1,1 +1,8 @@
 #ES 1 flexible calculator
+#print the greetings for the user, Welcome to the calculator. The avable operations would be: sum, avarage, max, min, product
+print("Welcome to the calculator")
+print("The avable operations would be: sum, avarage, max, min, product")
+num = []
+for num in num():
+    input("what would you like the numbers to be ")
+def input(*args):
