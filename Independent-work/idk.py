6b41ca4b4b9fe6f29d2dd0b2dd45405dd0622876
edwingsantos <edwing.santos@ucas-edu.net>
2025-12-11 18:30:54 -0700

    klashd

--- Independent-work/idk.py
+++ Independent-work/idk.py
@@ -38,6 +38,7 @@ info_gathered = {
 }
 places = ["bedroom", "school","friends house","kitchen", "cementary", "walmart","park","backyard"]
 
+
 def bedroom(stats):
     print("You are in your room.")
     actions_room = ["rest", "check phone", "drawing"]
@@ -87,6 +88,7 @@ def bedroom(stats):
             print("please select an actual option")
             continue
     return
+
 
 def school(stats):
     print("You have decided to go to school")
@@ -242,6 +244,7 @@ def school(stats):
             print("Maybe you spelled something wrong, you might wanna check again.")
             continue
 
+
 def friends_house(stats):
     print("You are in your friends house")
     actions_friendshouse = ["play video games","talk","start a fight"]
@@ -257,21 +260,27 @@ def friends_house(stats):
                 print("Happiness +10")
                 stats["happiness"] +=10
 
+
 def kitchen(stats):
     #Make the fight with your mom  the first time you enter the kitchen , every time you enter it you have to fight, but you get more resources
     print("You have entered the the kitchen")
 
+
 def cementary(stats):
     print("You have entered the cementary")
+
 
 def walmart(stats):
     print("You have entered walmart")
 
+
 def park(stats):
     print("You are at the park")
 
+
 def backyard(stats):
     print("You are in your backyard")
+
 
 print("This game is about you as a teenage self, the goal of this game is to win an argument against your ex in a text message fight about whose fault it was about the break up. In this game you will go through what it feels to be a Ucas student, you have a crazy mom and a crazy ex that is still bugging you about the break up. The obstacles in this game are keeping your grades acceptable, having a social life, doing most of your chores around the house, and dealing with the people in your life. You win this game by defeating the final boss, your ex, the two of you are going to be in an argument of whose fault was it about the break up, the life would be measured as emotional damage, the better you handle the obstacles, the less emotional damage you have, (by the way, if you read this i owe you  a penny) during the fight you can bring up stuff your ex did to you or how bad she was, you can find these useful things in the rooms around the house hidden. Finally, the game would give you options to choose from, and you have to choose the best one. Good luck and hope you live. :)")
 while True:
@@ -293,7 +302,7 @@ while True:
         elif where == "kitchen":
             kitchen(stats)
         elif where == "cementary":
-            cementary(stats)   
+            cementary(stats)  
         elif where == "walmart":
             walmart(stats)
         elif where == "part":
@@ -303,3 +312,4 @@ while True:
         else:
             print("You maybe spelled something wrong, please re-type")
             continue
+
